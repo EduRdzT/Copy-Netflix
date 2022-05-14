3e1d27d8c167eb6709fbde157a769a99991916a1
Edrotz <eduardo_rdzt@outlook.com>
2022-05-13 19:27:26 -0500

    key.js

--- src/helpers/stripe-checkout.js
+++ src/helpers/stripe-checkout.js
@@ -1,5 +1,7 @@
-export default {
+const TMDB_KEYS = {
   keyApi: "3236e11739b44c56fbc857bade3c0a00",
   accessToken:
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjM2ZTExNzM5YjQ0YzU2ZmJjODU3YmFkZTNjMGEwMCIsInN1YiI6IjYyNzQ3NWZlN2NmZmRhNzMxNmM5N2ZiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N_e0IuIGI0EsAOXvJANqY3PK21eawTeUXQrs52p3Ap8",
 };
+
+export default TMDB_KEYS;
