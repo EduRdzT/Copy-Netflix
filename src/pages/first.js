cb8c5ae1f3e945a080696a5b5c47f2da804b15fc
Edrotz <eduardo_rdzt@outlook.com>
2022-05-23 14:04:56 -0500

    first page css

--- src/pages/first.js
+++ src/pages/first.js
@@ -3,13 +3,27 @@ import React from "react";
 const First = () => {
   return (
     <main className="login">
-      <h3>Películas y series ilimitadas y mucho más.</h3>
-      <h4>Disfruta donde quieras. Cancela cuando quieras.</h4>
-      <p>¿Quieres ver Netflix ya? Ingresa tu email</p>
-      <p>para crear una cuenta o reiniciar tu</p>
-      <p>membresía de Netflix</p>
-      <input type="text" placeholder="Email" />
-      <button className="sign">Comenzar</button>
+      <div className="first-page">
+        <h3>Películas y series</h3>
+        <h3>ilimitadas y mucho más.</h3>
+        <h4>Disfruta donde quieras. Cancela cuando quieras.</h4>
+        <p>
+          ¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o
+          reiniciar tu membresía de Netflix
+        </p>
+        <div>
+          <div className="form-email">
+            {/* <label className="label"> */}
+            <input type="email" placeholder=" " />
+            <p>Email</p>
+            {/* </label> */}
+          </div>
+          <button className="sign">
+            Comenzar
+            <span className="material-symbols-outlined">navigate_next</span>
+          </button>
+        </div>
+      </div>
     </main>
   );
 };
