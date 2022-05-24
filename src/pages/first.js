edeec98faa9a6f320ed0cc8a873676915839b7a8
Edrotz <eduardo_rdzt@outlook.com>
2022-05-24 12:32:57 -0500

    validacion

--- src/pages/first.js
+++ src/pages/first.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const initial = {
+  ok: false,
+  message: "El email es obligatorio",
+};
 
 const First = () => {
+  const [form, setForm] = useState({});
+  const [wrong, setWrong] = useState(initial);
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  useEffect(() => {
+    const regex = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+    if (!form.email) {
+      setWrong(initial);
+      return;
+    }
+    if (!regex.test(form.email)) {
+      setWrong({ ok: false, message: "Email incorrecto" });
+    } else {
+      setWrong({ ok: true, message: "" });
+    }
+  }, [form]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(wrong.message);
+    if (wrong.ok) {
+      alert("El formulario se ha enviado");
+    }
+  };
+
   return (
     <main className="login">
       <div className="first-page">
@@ -11,18 +48,23 @@ const First = () => {
           ¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o
           reiniciar tu membresía de Netflix
         </p>
-        <div>
-          <div className="form-email">
-            {/* <label className="label"> */}
-            <input type="email" placeholder=" " />
+        <form>
+          <label className="form-email">
+            <input
+              type="email"
+              name="email"
+              value={form.nombre}
+              onBlur={handleChange}
+              placeholder=" "
+            />
             <p>Email</p>
-            {/* </label> */}
-          </div>
-          <button className="sign">
+          </label>
+          <button className="sign" onClick={handleSubmit}>
             Comenzar
             <span className="material-symbols-outlined">navigate_next</span>
           </button>
-        </div>
+          <span>{message}</span>
+        </form>
       </div>
     </main>
   );
